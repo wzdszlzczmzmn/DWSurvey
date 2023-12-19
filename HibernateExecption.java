9c71d6893167397666e8f90032840c66be45a56a
Rel1sh1 <149125916+Rel1sh1@users.noreply.github.com>
2023-12-19 21:09:59 +0800

    Delete HibernateExecption.java

--- HibernateExecption.java
+++
@@ -1,7 +0,0 @@
-package net.diaowen.common.exception;
-
-public class HibernateExecption extends RuntimeException{
-    public HibernateExecption(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
