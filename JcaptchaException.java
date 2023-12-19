81f4d50c7e72085c6235be21bebd684433795f8c
Rel1sh1 <149125916+Rel1sh1@users.noreply.github.com>
2023-12-19 21:10:09 +0800

    Delete JcaptchaException.java

--- JcaptchaException.java
+++
@@ -1,9 +0,0 @@
-package net.diaowen.common.exception;
-
-import java.io.IOException;
-
-public class JcaptchaException extends IOException {
-    public JcaptchaException(String message) {
-        super(message);
-    }
-}
