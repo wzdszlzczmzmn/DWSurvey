d67de4cb2b23003d3579e0ead5338f8327982e30
Rel1sh1 <149125916+Rel1sh1@users.noreply.github.com>
2023-12-19 21:08:39 +0800

    Add files via upload

---
+++ HibernateExecption.java
@@ -0,0 +1,7 @@
+package net.diaowen.common.exception;
+
+public class HibernateExecption extends RuntimeException{
+    public HibernateExecption(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ JcaptchaException.java
@@ -0,0 +1,9 @@
+package net.diaowen.common.exception;
+
+import java.io.IOException;
+
+public class JcaptchaException extends IOException {
+    public JcaptchaException(String message) {
+        super(message);
+    }
+}
